2b2f8bc05bcef142c311726cf7a782d04c96a338
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-03-22 22:59:06 +0000

    code for @getvid_

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
 # GetVideoBot
-Twitter bot to download videos and gifs.
-Bot can be found at https://twitter.com/GetvideoBot
+Twitter bot to download videos and gifs.  
+Bot can be found at [https://twitter.com/GetVid_](https://twitter.com/GetVid_)
 
-How it works
-1.	User finds a tweet containing a video or gif.
-2.	User mentions @GetVideoBot in a reply
-3.	The bot replies with a link to download the video.
+### How it works  
+1.	User finds a tweet containing a video or gif.  
+2.	User mentions @GetVid_ in a reply  
+3.	The bot replies with a link to download the video.  
+
+Replace `src/main/resources/settings.properties.example` with `src/main/resources/settings.properties`.  
+```
+cp src/main/resources/settings.properties.example src/main/resources/settings.properties
+```
+
+The settings are stored inside the `src/main/resources/settings.properties` file.  
+Edit the settings.properties file and insert your bot username and Twitter API keys.  
+
+### Twitter API
+Replace the value of `twitter.username=` property with your Twitter bot username. Make sure to include the @ before it.  
+
+Replace the value of the following properties with your Twitter Developer API credentials (https://developer.twitter.com/en)  
+```
+tw.apiKey=
+tw.apiSecretKey=
+tw.accessToken=
+tw.accessTokenSecret=
+```
+
+### To build and compile the program   
+`mvn clean install`
+
+After a successful build, you can start the program with:  
+```
+java -jar target/getvid-2.0.jar
+```
 
 
-This readme will be updated when i have time.
+Use `nohup` to keep the program running even after the terminal is closed:
+```
+nohup java -jar target/getvid-2.0.jar &
+```
--- src/main/java/Stream.java
+++ src/main/java/Stream.java
@@ -4,17 +4,11 @@ import io.github.redouane59.twitter.dto.stream.StreamRules;
 import io.github.redouane59.twitter.dto.tweet.Tweet;
 import io.github.redouane59.twitter.dto.tweet.TweetParameters;
 import io.github.redouane59.twitter.dto.tweet.TweetType;
-import io.github.redouane59.twitter.dto.tweet.TweetV2;
-import utils.DBHelper;
 import utils.ReadProperty;
 import utils.Utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Stream {
 
@@ -24,27 +18,27 @@ public class Stream {
 
         String botUsername = ReadProperty.getValue("twitter.username");
 
-            try {
-                List<StreamRules.StreamRule> rules = twitter.retrieveFilteredStreamRules();
+        try {
+            List<StreamRules.StreamRule> rules = twitter.retrieveFilteredStreamRules();
 
-                if (rules == null) {
-                    twitter.addFilteredStreamRule(botUsername, "");
-                } else {
+            if (rules == null) {
+                twitter.addFilteredStreamRule(botUsername, "");
+            } else {
 
-                    for (StreamRules.StreamRule rule : rules) {
-                        if (!Objects.equals(rule.getValue(), botUsername)) {
-                            twitter.deleteFilteredStreamRuleId(rule.getId());
-                            twitter.addFilteredStreamRule(botUsername, "");
-                        }
+                for (StreamRules.StreamRule rule : rules) {
+                    if (!Objects.equals(rule.getValue(), botUsername)) {
+                        twitter.deleteFilteredStreamRuleId(rule.getId());
+                        twitter.addFilteredStreamRule(botUsername, "");
                     }
                 }
-
-                System.out.println("Stream connected to: " + botUsername);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("Exception: " + e.getMessage());
             }
+
+            System.out.println("Stream connected to: " + botUsername);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception: " + e.getMessage());
+        }
 
         twitter.startFilteredStream(new IAPIEventListener() {
             @Override
@@ -57,7 +51,6 @@ public class Stream {
             public void onTweetStreamed(Tweet tweet) {
                 try {
                     String tweetReferencedId = "";
-                    String mentionTweetId = tweet.getId();
 
                     if (tweet.getTweetType().equals(TweetType.QUOTED)) {
                         tweetReferencedId = tweet.getInReplyToStatusId();
@@ -73,50 +66,26 @@ public class Stream {
 
                         if (mediaTweet.getMedia() != null && !mediaTweet.getMedia().isEmpty() && mediaTweet.getMedia().get(0).getType().equals("video")) {
 
-                            TweetV2.MediaEntityV2 mediaEntity = (TweetV2.MediaEntityV2) mediaTweet.getMedia().get(0);
-                            String thumbnail = mediaEntity.getPreviewImageUrl();
-                            String mediaTweetUser = mediaTweet.getUser().getName();
-                            String mediaTweetText = mediaTweet.getText();
-                            String isSensitive = ""; // temporary
-                            String videoUrl = ""; // don't really need it
+                            String userToReply = tweet.getUser().getName();
+                            String responseText = tweetMessage(tweetReferencedId);
 
-                            boolean shouldReply = true;
+                            try {
+                                replyTweet(responseText, tweet.getId(), userToReply);
+                            } catch (Exception e) {
 
-                            // fix reply bug - a case were the bot replies where it is not intentionally called
-                            // on its own video post or retweeted video posts
+                                if (e.getMessage().contains("User is over daily status update limit")) {
+                                    System.out.println("limit reached");
 
-                            if (mediaTweetUser.equalsIgnoreCase(botUsername.split("@")[1]) // get username without @ symbol
-                                    || mediaTweetUser.equalsIgnoreCase("crazyvideoclips")) {
-                                // get number of times the bot appears in a tweet
-                                if (getMentionCount(tweet.getText(), botUsername.toLowerCase()) == 1) {
-                                    shouldReply = false;
+                                } else {
+                                    System.out.println(e.getMessage());
                                 }
+
                             }
-
-                            if (shouldReply) {
-                                DBHelper.saveTweet(tweet.getUser().getName(), tweet.getId(), mediaTweet.getId(), videoUrl, thumbnail, mediaTweetUser, mediaTweetText, isSensitive);
-
-                                String userToReply = tweet.getUser().getName();
-                                String responseText = tweetMessage(userToReply);
-
-                                try {
-                                    replyTweet(responseText, tweet.getId(), userToReply);
-                                } catch (Exception e) {
-
-                                    if (e.getMessage().contains("User is over daily status update limit")) {
-                                        System.out.println("limit reached");
-
-                                    } else {
-                                        System.out.println(e.getMessage());
-                                    }
-                                }
-                            }
-
 
                         }
-
                     }
-                } catch (Exception e) {
+                } catch (
+                        Exception e) {
                     e.printStackTrace();
                 }
 
@@ -135,33 +104,16 @@ public class Stream {
 
     }
 
-    public static int getMentionCount(String tweetText, String username) {
-        String regex_url = "(?<=^|(?<=[^a-zA-Z0-9-_\\.]))@([A-Za-z]+[A-Za-z0-9_]+)";
-        int count = 0;
-
-        Pattern pattern = Pattern.compile(regex_url);
-        Matcher matcher = pattern.matcher(tweetText.toLowerCase());
-
-        while (matcher.find()) {
-            if (matcher.group(0).equalsIgnoreCase(username)) {
-                count += 1;
-            }
-        }
-
-        return count;
+    private static String getLink(String tweetId) {
+        return ReadProperty.getValue("website.url") + "/video/" + tweetId;
     }
 
-    private static String getUserPage(String username) {
-        String userPage = ReadProperty.getValue("website.url") + "/" + username;
-        String notice = "\n\nAd: Follow @CrazyVideoClips for crazy and viral videos.";
-        return userPage + notice;
-    }
 
     private static String tweetMessage(String username) {
         String[] messages = {"Yes! video, it's here: ", "Alright, i got this: ", "Video? Here we go: ", "Yes! video is ready: ", "Yes, Video! At your service: "};
         int rand = (int) (messages.length * Math.random());
 
-        return messages[rand] + getUserPage(username);
+        return messages[rand] + getLink(username);
     }
 
     private static void replyTweet(String text, String inReplyToTweetId, String whoMentionedMe) {
